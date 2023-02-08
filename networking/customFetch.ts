eae9584cb74ddb4d1ec0dee29d99bcd7cd2bec5e
Michael DeGori <michael.degori@gmail.com>
2023-02-08 14:23:10 -0500

    more robust api errors

--- networking/customFetch.ts
+++ networking/customFetch.ts
@@ -1,5 +1,6 @@
 const defaultOptions = {
 	retryCount: 4,
+	retryDelay: 1000,
 	defaultValue: null,
 };
 
@@ -7,6 +8,7 @@ export const customFetch = async (
 	url: string,
 	options: {
 		retryCount?: number;
+		retryDelay?: number;
 		defaultValue?: any;
 	} = {}
 ) => {
@@ -17,13 +19,26 @@ export const customFetch = async (
 			console.log(`Trying to fetch from ${url} ...`);
 			const response = await fetch(url);
 
-			if (response.status < 200 || response.status > 299)
-				throw new Error();
+			if (response.status >= 400) {
+				console.error(
+					`Request failed with status code ${response.status}`
+				);
+				throw new Error(`Status code ${response.status}`);
+			}
 
 			const data = await response.json();
 			return data;
-		} catch (err) {
-			console.error(`Fetch ${retry + 1} failed`);
+		} catch (err: any) {
+			console.error(`Fetch ${retry + 1} failed: ${err.message}`);
+
+			if (retry + 1 < resolvedOptions.retryCount) {
+				console.log(
+					`Waiting ${resolvedOptions.retryDelay}ms before retrying...`
+				);
+				await new Promise(resolve =>
+					setTimeout(resolve, resolvedOptions.retryDelay)
+				);
+			}
 		}
 	}
 
